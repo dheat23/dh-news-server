765f074df0f6b765cb2971b8449d50a949a900fe
dheat23 <domsredirects@gmail.com>
2023-11-20 11:29:10 +0000

    add error handler functions

--- app/app.js
+++ app/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const { getAllTopics } = require("./controllers");
+const { handlePsqlErrors, handleCustomErrors, handleServerErrors } = require("./errors");
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/topics", getAllTopics);
+
+app.use(handlePsqlErrors);
+app.use(handleCustomErrors);
+app.use(handleServerErrors);
 
 
 module.exports = app
