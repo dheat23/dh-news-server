dc7eebeaa760bd0c44a38145eec7a58234460222
dheat23 <domsredirects@gmail.com>
2023-11-22 09:59:29 +0000

    add comment_count feature to selectArticleById

--- app/models.js
+++ app/models.js
@@ -8,7 +8,7 @@ exports.selectAllTopics = () => {
 };
 
 exports.selectArticleById = (id) => {
-    return db.query(`SELECT * FROM articles WHERE article_id = $1;`, [id])
+    return db.query(`SELECT articles.author, title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.body, article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id;`, [id])
     .then(({rows}) => {
         if (rows.length === 0) {
             return Promise.reject({status: 404, msg: "Article does not exist"})
